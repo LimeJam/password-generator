d27df68210b0e9536aa56f52d3a13aece6ac1b3d
Nikita Lukianets <nikita@limejam.com>
2019-03-17 22:59:39 +0100

    experiments with balanced character set, poor performance

--- src/js/premutatio.js
+++ src/js/premutatio.js
@@ -1,42 +1,50 @@
-var pgLength = document.getElementById("pgLength").value;
-
-var Lowercase = 0;
-if (document.getElementById("Lowercase").checked) {
-    Lowercase = 1
-}
-var Uppercase = 0;
-if (document.getElementById("Uppercase").checked) {
-    Uppercase = 1
-}
-var Numbers = 0;
-if (document.getElementById("Numbers").checked) {
-    Numbers = 1
-}
-var Symbols = 0;
-if (document.getElementById("Symbols").checked) {
-    Symbols = 1
-}
-var Alike = 0;
-if (document.getElementById("Alike").checked) {
-    Alike = 1
-}
-var Ambiguous = 0;
-if (document.getElementById("Ambiguous").checked) {
-    Ambiguous = 1
+charactersJSON = {
+    "Lowercase": "abcdefghjkmnpqrstuvwxyz",
+    "Uppercase": "ABCDEFGHJKLMNPQRSTUVWXYZ",
+    "Numbers": "23456789",
+    "Symbols": "!#$%&*+-=?@^_",
+    "Alike": "ilo|IO01",
+    "Ambiguous": "{}[]()\/'\"`~,;:.<>\\"
 }
 
-var dictionary = dictionary(Lowercase,Uppercase,Numbers,Symbols,Alike,Ambiguous);
 
-function dictionary(Lowercase,Uppercase,Numbers,Symbols,Alike,Ambiguous) {
+function computeSetRepeat(set, charactersJSON) {
+/**
+ *  computeFraction(set, charactersJSON) {
+ * set - string
+ * charactersJSON -  JSON with key:value pairs of character sets
+*/
+    //safe defaults
+    if (typeof(set)==='undefined') {
+        return ""
+    }
+    else {
+        var denominator = 1;
+        for (var i in charactersJSON) {
+            denominator*=charactersJSON[i].length;
+
+            console.log(charactersJSON[i]);
+            console.log(denominator);
+        }
+        var repeat = Math.floor(denominator/set.length);
+
+        var setRepeat = "";
+        for (var j = 0; j<repeat; i++) {
+            setRepeat+=set;
+        }
+
+        return setRepeat
+    }
+}
+function computeDictionary(Lowercase,Uppercase,Numbers,Symbols,Alike,Ambiguous,balanced) {
     var dictionary = "";
 
-
-    if (Lowercase==1) {dictionary+="abcdefghjkmnpqrstuvwxyzabcdefghjkmnpqrstuvwxyz";}
-    if (Uppercase==1) {dictionary+="ABCDEFGHJKLMNPQRSTUVWXYZABCDEFGHJKLMNPQRSTUVWXYZ";}
-    if (Numbers==1) {dictionary+="234567892345678923456789234567892345678923456789";}
-    if (Symbols==1) {dictionary+="!#$%&*+-=?@^_!#$%&*+-=?@^_!#$%&*+-=?@^_!#$%&*+-=?@^_";}
-    if (Alike==1) {dictionary+="ilo|IO01ilo|IO01ilo|IO01ilo|IO01ilo|IO01ilo|IO01ilo|IO01";}
-    if (Ambiguous==1) {dictionary+="{}[]()\/'\"`~,;:.<>\\{}[]()\/'\"`~,;:.<>\\{}[]()\/'\"`~,;:.<>\\"}
+    if (Lowercase) {dictionary+="abcdefghjkmnpqrstuvwxyzabcdefghjkmnpqrstuvwxyz";}
+    if (Uppercase) {dictionary+="ABCDEFGHJKLMNPQRSTUVWXYZABCDEFGHJKLMNPQRSTUVWXYZ";}
+    if (Numbers) {dictionary+="234567892345678923456789234567892345678923456789";}
+    if (Symbols) {dictionary+="!#$%&*+-=?@^_!#$%&*+-=?@^_!#$%&*+-=?@^_!#$%&*+-=?@^_";}
+    if (Alike) {dictionary+="ilo|IO01ilo|IO01ilo|IO01ilo|IO01ilo|IO01ilo|IO01ilo|IO01";}
+    if (Ambiguous) {dictionary+="{}[]()\/'\"`~,;:.<>\\{}[]()\/'\"`~,;:.<>\\{}[]()\/'\"`~,;:.<>\\";}
 
     return dictionary
 }
@@ -51,6 +59,38 @@ function premutatio(pgLength,dictionary) {
     if (dictionary == '') {return "Please select at least one character set"}
     return premutatio
 }
+
+//computed onLoad
 function setPwd() {
+    var Lowercase = false;
+    if (document.getElementById("Lowercase").checked) {
+        Lowercase = true;
+    }
+    var Uppercase = false;
+    if (document.getElementById("Uppercase").checked) {
+        Uppercase = true;
+    }
+    var Numbers = false;
+    if (document.getElementById("Numbers").checked) {
+        Numbers = true;
+    }
+    var Symbols = false;
+    if (document.getElementById("Symbols").checked) {
+        Symbols = true;
+    }
+    var Alike = false;
+    if (document.getElementById("Alike").checked) {
+        Alike = true;
+    }
+    var Ambiguous = false;
+    if (document.getElementById("Ambiguous").checked) {
+        Ambiguous = true;
+    }
+
+    //commented out due to poor performance computeSetRepeat("abc",charactersJSON);
+
+    var pgLength = document.getElementById("pgLength").value;
+    var dictionary = computeDictionary(Lowercase,Uppercase,Numbers,Symbols,Alike,Ambiguous);
+
     document.getElementById("final_pass").value = premutatio(pgLength,dictionary);
 }
